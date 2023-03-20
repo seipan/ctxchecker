ddbfdf1ae96c89497779b22a5cc185de1b4c1f3c
seipan <yamasakipann0218@gmail.com>
2023-03-21 00:03:51 +0900

    init

--- ctxchecker.go
+++ ctxchecker.go
@@ -2,6 +2,7 @@ package ctxchecker
 
 import (
 	"go/ast"
+	"strconv"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -22,12 +23,9 @@ var Analyzer = &analysis.Analyzer{
 
 func run(pass *analysis.Pass) (any, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	_ = getCommentMap(pass)
 
-	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
-	}
-
-	inspect.Preorder(nodeFilter, func(n ast.Node) {
+	inspect.Preorder(nil, func(n ast.Node) {
 		switch n := n.(type) {
 		case *ast.Ident:
 			if n.Name == "gopher" {
@@ -37,4 +35,19 @@ func run(pass *analysis.Pass) (any, error) {
 	})
 
 	return nil, nil
+}
+
+func getCommentMap(pass *analysis.Pass) map[string]string {
+	var mp = make(map[string]string)
+
+	for _, file := range pass.Files {
+		for _, cg := range file.Comments {
+			for _, c := range cg.List {
+				pos := pass.Fset.Position(c.Pos())
+				mp[pos.Filename+"_"+strconv.Itoa(pos.Line)] = c.Text
+			}
+		}
+	}
+
+	return mp
 }
